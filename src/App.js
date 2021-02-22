d92b767993ce8b912c332b76d64cb2bc6061f638
Adams Omorede <adamsomorede96@gmail.com>
2021-02-22 12:38:48 +0000

    added player function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ function App() {
       </header>
     );
   }
+
+  const Player = () => {
+    return(
+      <div className="player">
+        <span className ="player-name">
+          Adams
+        </span>
+        <div className="counter">
+          <button className="counter-action decrement"> - </button>
+          <span className="counter-score">35</span>
+          <button className="counter-action increment"> + </button>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
