bda71a79d310e2528ee476410e1d1665aabd5204
Adams Omorede <adamsomorede96@gmail.com>
2021-02-22 16:57:50 +0000

    Added a nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import './semantic/dist/semantic.css';
+import navBar from './components/navbar';
 
-const navBar = 'nav';
 const title = <h1 className="Title">Bottoms Up!</h1>
 const players = <h1 className="players">Player</h1>
 
 function App() {
   return(
-  <div className={navBar}>
-    <span>{title}</span>
-  </div>
-
+  <div>{navBar}</div>
   );
 
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+const navBar = 
+    <div class="ui menu">
+    <div class="header item">
+      Bottoms up!
+    </div>
+    <a class="item">
+      About Us
+    </a>
+    <a class="item">
+      Jobs
+    </a>
+    <a class="item active">
+      Locations
+    </a>
+  </div> 
+
+export default navBar;
